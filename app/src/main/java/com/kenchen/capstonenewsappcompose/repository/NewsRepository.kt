895526a4d4fe5a95a4aeae19d8faca76d16a4710
KenChen <ken22885@gmail.com>
2022-12-16 11:43:41 +0800

    Create NewsRepository and implementaion

---
+++ app/src/main/java/com/kenchen/capstonenewsappcompose/repository/NewsRepository.kt
@@ -0,0 +1,21 @@
+package com.kenchen.capstonenewsappcompose.repository
+
+import com.kenchen.capstonenewsappcompose.model.Article
+import com.kenchen.capstonenewsappcompose.model.ArticleState
+import com.kenchen.capstonenewsappcompose.model.Source
+import kotlinx.coroutines.flow.Flow
+
+interface NewsRepository {
+
+    fun getArticles(): Flow<ArticleState>
+
+    suspend fun addArticles(articles: List<Article>)
+
+    suspend fun clearArticles()
+
+    suspend fun searchArticles(search: String): List<Article>
+
+    suspend fun getSources(): List<Source>
+
+    suspend fun addSources(sources: List<Source>)
+}
---
+++ app/src/main/java/com/kenchen/capstonenewsappcompose/repository/NewsRepositoryImp.kt
@@ -0,0 +1,89 @@
+package com.kenchen.capstonenewsappcompose.repository
+
+import android.util.Log
+import com.kenchen.capstonenewsappcompose.database.dao.ArticleDao
+import com.kenchen.capstonenewsappcompose.database.dao.SourceDao
+import com.kenchen.capstonenewsappcompose.model.Article
+import com.kenchen.capstonenewsappcompose.model.ArticleState
+import com.kenchen.capstonenewsappcompose.model.Source
+import com.kenchen.capstonenewsappcompose.networking.NetworkStatusChecker
+import com.kenchen.capstonenewsappcompose.networking.RemoteApiImp
+import com.kenchen.capstonenewsappcompose.utils.mapException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class NewsRepositoryImp @Inject constructor(
+    private val articleDao: ArticleDao,
+    private val sourceDao: SourceDao,
+    private val remoteApi: RemoteApiImp,
+    private val networkStatusChecker: NetworkStatusChecker,
+) : NewsRepository {
+
+    companion object {
+        private const val TAG = "NewsRepositoryImpl"
+    }
+
+    override fun getArticles(): Flow<ArticleState> {
+        return flow<ArticleState> {
+            // can emit ArticleState.loading() here for loading state
+            // always get from local Room database first
+            val newsArticles = articleDao.getArticles()
+
+            emit(ArticleState.Ready(newsArticles))
+
+            Log.i(TAG, "Articles from local database size = ${newsArticles.size}")
+
+
+            val isOnWifi = networkStatusChecker.isOnWifiConnection()
+
+            println(isOnWifi)
+            // only fetch the data when
+            // 1. there is wifi connection
+            // 2. unclick the has wifi on menu item, means user can use data without wifi
+
+            try {
+                println("success")
+                val result = remoteApi.getTopHeadlinesByCountry("us")
+                println("success1")
+
+                emit(ArticleState.Ready(result))
+                println("success2")
+
+                // if network data is not empty, means the source of truth
+                // add to database
+                if (result.isNotEmpty()) {
+                    println("success3")
+
+                    // clear first to prevent stacking up old articles
+                    articleDao.clearArticles()
+                    articleDao.addArticles(result)
+                }
+                println("success4")
+
+
+            } catch (error: Exception) {
+                println("Throw error")
+
+                // emit partial to show that data is coming from Room database not from
+                // remote API
+                emit(ArticleState.Partial(newsArticles, mapException(error)))
+                // log the error for team to inspect
+                Log.e(TAG, "Articles from local database network error")
+            }
+
+
+        }
+    }
+
+    override suspend fun searchArticles(search: String): List<Article> = articleDao
+        .searchArticles(search)
+
+    override suspend fun addArticles(articles: List<Article>) = articleDao.addArticles(articles)
+
+    override suspend fun clearArticles() = articleDao.clearArticles()
+
+    override suspend fun getSources(): List<Source> = sourceDao.getSources()
+
+    override suspend fun addSources(sources: List<Source>) = sourceDao.addSources(sources)
+}
