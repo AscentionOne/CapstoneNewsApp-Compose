44ab465f3c56d83a9d71c402378883f2903eeaf2
KenChen <ken22885@gmail.com>
2022-12-23 11:03:25 +0800

    Add retrieve article by title functionality

--- app/src/main/java/com/kenchen/capstonenewsappcompose/database/dao/ArticleDao.kt
+++ app/src/main/java/com/kenchen/capstonenewsappcompose/database/dao/ArticleDao.kt
@@ -10,14 +10,17 @@ import com.kenchen.capstonenewsappcompose.model.Article
 interface ArticleDao {
 
     @Query("SELECT * FROM article")
-    suspend fun getArticles():List<Article>
+    suspend fun getArticles(): List<Article>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addArticles(articles:List<Article>)
+    suspend fun addArticles(articles: List<Article>)
 
     @Query("DELETE FROM article")
     suspend fun clearArticles()
 
     @Query("SELECT * FROM article WHERE title LIKE :search")
-    suspend fun searchArticles(search:String): List<Article>
+    suspend fun searchArticles(search: String): List<Article>
+
+    @Query("SELECT * FROM article WHERE title = :title")
+    suspend fun retrieveArticleByTitle(title: String): Article
 }
